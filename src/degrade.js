7b0dd88dfb79cf033888fa20c1f70ea363c0c11e
Dumas-Lavoie <goglu1@live.ca>
2020-09-18 14:07:58 -0400

    TEST

---
+++ src/degrade.js
@@ -0,0 +1,25 @@
+
+
+
+
+    var granimInstance = new Granim({
+        element : '#degra_canv',
+        name : 'granim',
+        opacity : [1,0.5],
+        states : {
+            "default-state": {
+                gradients: [
+                    [
+                        { color: '#0D3750', pos: .3 },
+                        { color: '#EB5C5E', pos: .9 }
+                     ],
+
+                     [
+                        { color: '#0D3750', pos: .1 },
+                        { color: '#EB5C5E', pos: .6 }
+                     ]
+                     // Optionne
+                ]
+            }
+        }
+    })
